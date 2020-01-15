3807380849da66492e5e6d2dc2ec6388b872e21c
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-01-15 07:59:44 -0300

    aula002

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-//get,post, put, delete
+mongoose.connect('mongodb+srv://oministack:F3rn4nd0@cluster0-axa2g.mongodb.net/week10?retryWrites=true&w=majority',{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
+
+app.use(express.json());
+
+// Metodos HTTP: get,post, put, delete
 
 app.get('/',(request,response)=>{
     return response.json({message: 'Hello OminiStack'});
